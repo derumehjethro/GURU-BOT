36367b523c9317090c67bb05cca3e81222ff36c1
Ajayi Marvellous <ajayimarvellous777@gmail.com>
2023-11-19 20:09:01 +0100

    added desmond as owner

--- config.js
+++ config.js
@@ -10,12 +10,12 @@ global.owner = [
   ['2348034255019', 'Marvel', true],
   ['2349122920562', 'CodeyBot', true],
   ['2348101331527', 'OB', true],
-  [''] 
+  ['2347036080471', 'Desmond', true],
 ] //Number of owners
 
-global.mods = ['2348034255019', '2349122920562', '2348101331527'] 
-global.prems = ['2348034255019', '2349122920562', '2348101331527']
-global.allowed = ['2348034255019', '2349122920562', '2348101331527']
+global.mods = ['2348034255019', '2349122920562', '2348101331527', '2347036080471'] 
+global.prems = ['2348034255019', '2349122920562', '2348101331527', '2347036080471']
+global.allowed = ['2348034255019', '2349122920562', '2348101331527', '2347036080471']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
