64efb3ad915a08c6392274b36b0384aec96f92f3
Ajayi Marvellous <ajayimarvellous777@gmail.com>
2023-12-22 02:15:47 +0100

    remove unnessary stuff

--- plugins/GURU-Openai.js
+++ plugins/GURU-Openai.js
@@ -12,8 +12,7 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
   try {
     m.react(rwait)
     const { key } = await conn.sendMessage(m.chat, {
-      image: { url: 'https://telegra.ph/file/c3f9e4124de1f31c1c6ae.jpg' },
-      caption: 'Thinking....'
+      text: 'Thinking....'
     }, {quoted: m})
     conn.sendPresenceUpdate('composing', m.chat);
     const prompt = encodeURIComponent(text);
