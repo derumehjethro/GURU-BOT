47dda127dc2bfab4fb4c6be9b225eb5205a0b9a0
Ajayi Marvellous <66161068+Marvellous890@users.noreply.github.com>
2024-02-16 11:11:58 +0000

    Refactor GURU-Openai.js plugin

--- plugins/GURU-Openai.js
+++ plugins/GURU-Openai.js
@@ -9,67 +9,16 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
     text = m.quoted.text;
   }
 
-  try {
-    m.react(rwait)
-    const { key } = await conn.sendMessage(m.chat, {
-      text: 'Thinking....'
-    }, {quoted: m})
-    conn.sendPresenceUpdate('composing', m.chat);
-    const prompt = encodeURIComponent(text);
+  const msg = encodeURIComponent(text);
 
-    const guru1 = `${gurubot}/chatgpt?text=${prompt}`;
-    
-    try {
-      let response = await fetch(guru1);
-      let data = await response.json();
-      let result = data.result;
+  const res = await fetch(`https://ultimetron.guruapi.tech/rekha?prompt=${msg}`);
 
-      if (!result) {
-        
-        throw new Error('No valid JSON response from the first API');
-      }
+  const json = await res.json();
 
-      await conn.relayMessage(m.chat, {
-        protocolMessage: {
-          key,
-          type: 14,
-          editedMessage: {
-            // imageMessage: { caption: result }
-            conversation: result
-          }
-        }
-      }, {});
-      m.react(done);
-    } catch (error) {
-      console.error('Error from the first API:', error);
 
-  
-      const model = 'llama';
-      const senderNumber = m.sender.replace(/[^0-9]/g, ''); 
-      const session = `GURU_BOT_${senderNumber}`;
-      const guru2 = `https://ultimetron.guruapi.tech/gpt3?prompt=${prompt}`;
-      
-      let response = await fetch(guru2);
-      let data = await response.json();
-      let result = data.completion;
+  let reply = json.result.response;
+  m.reply(reply);
 
-      await conn.relayMessage(m.chat, {
-        protocolMessage: {
-          key,
-          type: 14,
-          editedMessage: {
-            // imageMessage: { caption: result }
-            conversation: result
-          }
-        }
-      }, {});
-      m.react(done);
-    }
-
-  } catch (error) {
-    console.error('Error:', error);
-    throw `*ERROR*`;
-  }
 };
 handler.help = ['chatgpt']
 handler.tags = ['AI']
